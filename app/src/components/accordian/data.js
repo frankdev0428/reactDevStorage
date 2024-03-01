4aad1666152a1735cf3e16cc24cddbd5019b471b
frankdev0428@gmail.com <frankdev0428@gmail.com>
2024-02-29 17:39:28 -0800

    Implement muti selection

---
+++ app/src/components/accordian/data.js
@@ -0,0 +1,30 @@
+  const data = [
+    {
+      id : '1',
+      question: "Who was the first president of the United States?",
+      answer: 
+      "George Washington was the first president of the United States, serving from 1789 to 1797."  
+    },{
+        id : '2',
+        question: "Which president is known for issuing the Emancipation Proclamation?",
+        answer: 
+        "Abraham Lincoln issued the Emancipation Proclamation on January 1, 1863, during the American Civil War, declaring that all enslaved persons in Confederate territory were to be set free."  
+      },{
+        id : '3',
+        question: "Who was the only president to serve more than two terms in office?",
+        answer: 
+        "Franklin D. Roosevelt was the only president to serve more than two terms. He was elected to four terms and served from 1933 until his death in 1945."  
+      },{
+        id : '4',
+        question: "Which president signed the Civil Rights Act of 1964 into law?",
+        answer: 
+        "Lyndon B. Johnson signed the Civil Rights Act of 1964 into law, which outlawed discrimination based on race, color, religion, sex, or national origin."  
+      },{
+        id : '5',
+        question: "Who was the youngest person ever elected president of the United States?",
+        answer: 
+        "Theodore Roosevelt became the youngest person to assume the presidency at the age of 42, following the assassination of President William McKinley in 1901."  
+      },
+  ]
+
+  export default data;
--- app/src/components/accordian/index.jsx
+++ app/src/components/accordian/index.jsx
@@ -1,1 +1,33 @@
+//single selection
+//mutiple selection
+import data from './data';
+import { useState } from 'react';
+import './styles.css';
+export default function Accordian() {
+     
+   const [selected,setSelected] = useState(null);
 
+    function handleSingleSelection(getCurrentId) {
+       setSelected(getCurrentId === selected ? null : getCurrentId);
+    }
+    console.log(selected)
+    return <div className="wrapper">
+        <button className=''>Enable Muti Selection</button>
+        <div className="accordian">
+            {
+                data && data.length > 0 ?
+               data.map(dataItem => 
+               <div className="item">
+                <div onClick={() => handleSingleSelection(dataItem.id)} className="title">
+                  <h3>{dataItem.question}</h3>
+                  <span>+</span>
+                </div>
+                {
+                    selected === dataItem.id ?
+                    <div className='content'>{dataItem.answer}</div> : null 
+                }
+               </div>) : <div>No data found</div>
+            }
+        </div>
+       </div>
+}

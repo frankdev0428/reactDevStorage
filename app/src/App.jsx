70f02e8aeda7bf10ec47735d2ef2be07de16125c
frankdev0428@gmail.com <frankdev0428@gmail.com>
2024-03-15 10:15:22 -0700

    Implement load more button API fetching function

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,5 +1,6 @@
 import Accordian from "./components/accordian";
 import SliderImagesDup from "./components/duplicate";
+import LoadMoreData from "./components/load-more-images";
 import RandomColor from "./components/random-color";
 import ImageSlider from "./components/slider-images";
 import StarRating from "./components/star-rating";
@@ -15,8 +16,8 @@ function App() {
       {/* <StarRating noOfStar={10}/> */}
       {/* Image slider component */}
       {/* <ImageSlider url={"https://picsum.photos/v2/list"} limit={"10"} /> */}
-   
-      <SliderImagesDup url={"https://picsum.photos/v2/list"} limit={"10"}/>
+      {/* Load more product component */}
+      <LoadMoreData />
     </div>
   );
 }
